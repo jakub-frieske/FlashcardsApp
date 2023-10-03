86086c9295129daf40f1056e23dbe9a67c3918e7
jakub-frieske <frieske.jakub@gmail.com>
2023-10-04 01:42:49 +0200

    Fix and rename dto

---
+++ Src/FlashcardsApp/DTO/Exam/ExamHistoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FlashcardsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashcardsApp.ViewModels
+{
+    public class ExamHistoryViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Score { get; set; }
+        public string? Solved { get; set; }
+    }
+}
